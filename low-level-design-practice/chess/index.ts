5d3109e556017b2d0dd598e9e115495e1aa60b1e
Aravjit Sachdeva <aravjitsachdeva@Mac.attlocal.net>
2025-04-27 23:23:28 -0500

    feat: getting started with chess lld

---
+++ low-level-design-practice/chess/index.ts
@@ -0,0 +1,99 @@
+// entities in a chess game
+
+// board
+// 2-d array of 8X8
+// mark the positions on the board (includes the piece and the color)
+
+// functions needed
+// show eligible moves
+// perform move
+// start game
+// if the user selects a move, then  we must ensure no other piece exists on the move
+
+// pieces
+// 2 rook, 2 bishop, 2 knight, 1 king, 1 queen, 8 pawns
+
+enum ChessPieces {
+  rook1,
+  rook2,
+  knight1,
+  knight2,
+  bishop1,
+  bishop2,
+  king,
+  queen,
+  pawn1,
+  pawn2,
+  pawn3,
+  pawn4,
+  pawn5,
+  pawn6,
+  pawn7,
+  pawn8,
+  empty,
+}
+
+enum Color {
+  BLACK,
+  WHITE,
+  NONE,
+}
+
+interface Position {
+  row: number;
+  col: number;
+}
+
+type Square = {
+  piece: ChessPieces;
+  color: Color;
+};
+
+type ChessBoard = Square[][];
+
+export class ChessGame {
+  private board: ChessBoard;
+  private currentPlayer: Color;
+  private capturedPiecesBlack: [];
+  private capturedPiecesWhite: [];
+
+  constructor() {
+    this.currentPlayer = Color.WHITE;
+    this.board = this.initializeBoard();
+  }
+
+  private initializeBoard(): ChessBoard {
+    const board = [];
+
+    for (let i = 0; i < 8; i++) {
+      let row = [];
+      for (let j = 0; j < 8; j++) {
+        row.push({ piece: ChessPieces.empty, color: Color.NONE });
+      }
+      board.push(row);
+    }
+
+    return board;
+  }
+
+  displayBoard(): void {
+    console.log(this.board);
+  }
+
+  showEligibleMoves(piece: ChessPieces, position: board[][]): [][] {
+    // log all the elibible position for the given piece for the
+  }
+
+  performMove(piece, fromPosition, toPosition): void {
+    // validate fromPosition
+    // validate toPosition
+    // if position occupied by same color, throw error
+    // if position occupied by different color, call the cutPiece function
+    // update the board
+    // switch currentPlayer
+  }
+
+  cutPiece(position, piece): {};
+  // remove piece from the position
+  // add piece to the list of inactive pieces depending on the player
+}
